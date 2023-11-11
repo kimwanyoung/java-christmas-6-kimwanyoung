bc5695f303c5bfd425431786ed6da08e45ea270f
kimwanyoung <dhks2869@gmail.com>
2023-11-12 03:30:45 +0900

    test : 만원 미만 주문 혜택 미적용 테스트 및 테스트 코드 리팩토링

--- src/test/java/christmas/discount/DecemberDiscountEventsTest.java
+++ src/test/java/christmas/discount/DecemberDiscountEventsTest.java
@@ -1,9 +1,10 @@
 package christmas.discount;
 
-import static christmas.domain.EventName.CHRISTMAS_DISCOUNT;
-import static christmas.domain.EventName.GIFT_EVENT;
-import static christmas.domain.EventName.SPECIAL_DISCOUNT;
-import static christmas.domain.EventName.WEEKDAY_DISCOUNT;
+import static christmas.domain.menu.Foods.BARBECUE_RIB;
+import static christmas.domain.menu.Foods.CAESAR_SALAD;
+import static christmas.domain.menu.Foods.CHOCOLATE_CAKE;
+import static christmas.domain.menu.Foods.T_BONE_STEAK;
+import static christmas.domain.menu.Foods.ZERO_COKE;
 
 import christmas.domain.EventName;
 import christmas.domain.OrderedMenu;
@@ -19,44 +20,69 @@ import christmas.domain.menu.Foods;
 import java.util.HashMap;
 import java.util.Map;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class DecemberDiscountEventsTest {
 
-    private static OrderedMenu orderedMenu;
-
-    @BeforeAll
-    static void setUp() {
-        Map<Foods, Integer> menus = new HashMap<>();
-        menus.put(Foods.BARBECUE_RIB, 1);
-        menus.put(Foods.TAPAS, 1);
-        menus.put(Foods.T_BONE_STEAK, 1);
-        menus.put(Foods.CHOCOLATE_CAKE, 1);
-        orderedMenu = new OrderedMenu(menus);
-    }
-
     @Test
     @DisplayName("날짜에 적용되는 할인 테스트")
     void 적용되는_할인_테스트() {
-        //when
-        DecemberDiscountEvents decemberDiscountEvents = new DecemberDiscountEvents(new VisitDay(3),
-                new ChristmasDdayDiscount(EventName.CHRISTMAS_DISCOUNT),
-                new SpecialDayDiscount(EventName.SPECIAL_DISCOUNT),
-                new WeekdayDiscount(EventName.WEEKDAY_DISCOUNT, orderedMenu.getOrderedMenu()),
-                new WeekendDiscount(EventName.WEEKEND_DISCOUNT, orderedMenu.getOrderedMenu()),
-                new FreeGift(GIFT_EVENT, orderedMenu.calculateTotalAmount()));
+        // given
+        OrderedMenu orderedMenu = createOrderedMenu(T_BONE_STEAK, BARBECUE_RIB, ZERO_COKE,
+                CHOCOLATE_CAKE);
 
-        //when
+        // when
+        DecemberDiscountEvents decemberDiscountEvents = createDecemberDiscountEvents(3,
+                orderedMenu);
+
+        // when
         DiscountResultDto discountResultDto = decemberDiscountEvents.toDiscountResultDto(
                 orderedMenu.calculateTotalAmount());
         Map<EventName, Integer> discountsStatistics = discountResultDto.discountResult();
 
-        //then
-        Assertions.assertThat(discountsStatistics.containsKey(CHRISTMAS_DISCOUNT)).isTrue();
-        Assertions.assertThat(discountsStatistics.containsKey(WEEKDAY_DISCOUNT)).isTrue();
-        Assertions.assertThat(discountsStatistics.containsKey(SPECIAL_DISCOUNT)).isTrue();
-        Assertions.assertThat(discountsStatistics.containsKey(GIFT_EVENT)).isTrue();
+        // then
+        Assertions.assertThat(discountsStatistics).containsKeys(
+                EventName.CHRISTMAS_DISCOUNT,
+                EventName.WEEKDAY_DISCOUNT,
+                EventName.SPECIAL_DISCOUNT,
+                EventName.GIFT_EVENT);
+    }
+
+    @Test
+    @DisplayName("10000원 미만 주문시 혜택이 적용되지 않는다.")
+    void 만원_미만_주문_헤택_미적용_테스트() {
+        // given
+        OrderedMenu orderedMenu = createOrderedMenu(CAESAR_SALAD);
+
+        // when
+        DecemberDiscountEvents decemberDiscountEvents = createDecemberDiscountEvents(16,
+                orderedMenu);
+
+        // when
+        DiscountResultDto discountResultDto = decemberDiscountEvents.toDiscountResultDto(
+                orderedMenu.calculateTotalAmount());
+        Map<EventName, Integer> discountsStatistics = discountResultDto.discountResult();
+
+        // then
+        Assertions.assertThat(discountsStatistics).isEmpty();
+    }
+
+    private OrderedMenu createOrderedMenu(Foods... foods) {
+        Map<Foods, Integer> menus = new HashMap<>();
+        for (Foods food : foods) {
+            menus.put(food, 1);
+        }
+        return new OrderedMenu(menus);
+    }
+
+    private DecemberDiscountEvents createDecemberDiscountEvents(int day, OrderedMenu orderedMenu) {
+        return new DecemberDiscountEvents(
+                new VisitDay(day),
+                new ChristmasDdayDiscount(EventName.CHRISTMAS_DISCOUNT),
+                new SpecialDayDiscount(EventName.SPECIAL_DISCOUNT),
+                new WeekdayDiscount(EventName.WEEKDAY_DISCOUNT, orderedMenu.getOrderedMenu()),
+                new WeekendDiscount(EventName.WEEKEND_DISCOUNT, orderedMenu.getOrderedMenu()),
+                new FreeGift(EventName.GIFT_EVENT, orderedMenu.calculateTotalAmount()));
     }
 }
